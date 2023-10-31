77716be72721089eac1d8e402dafd477fcdcf42b
Will <wrs225@cornell.edu>
2023-10-30 20:53:47 -0700

    Inbuilt models and a semblance of tests

---
+++ models/vco.py
@@ -0,0 +1,34 @@
+import pythams.core.block as blocklib
+from pythams.core.expr import *
+import pythams.core.intervals as intervallib
+import pythams.core.fixedpoint as fxplib
+import pythams.core.integer as intlib 
+import pythams.core.rtl as rtllib 
+
+def generate_block(input_voltage = 3.3, rel_prec = 0.01, timestep = 1e-2):
+    vco = blocklib.AMSBlock("vco")
+    #w = vco.decl_input(Real("w"))
+    #out = vco.decl_output(Real("out"))
+    
+    gain = vco.decl_param("gain",Constant(0.75))
+    d    = vco.decl_param("damping_resistance",Constant(0.0036))
+
+    w = vco.decl_var("w", kind=blocklib.VarKind.Input, type=RealType(0,3.3,prec=rel_prec)) #total kludge to get the product lower above 0.00
+    x = vco.decl_var("x", kind=blocklib.VarKind.StateVar, type=RealType(lower=-3.3,upper=3.3,prec=rel_prec))
+    v = vco.decl_var("v", kind=blocklib.VarKind.StateVar, type=RealType(lower=-3.3,upper=3.3,prec=rel_prec))
+
+    out = vco.decl_var("out", kind=blocklib.VarKind.Output, type=x.type)
+    dvdt = vco.decl_var("dvdt", kind=blocklib.VarKind.Transient,  \
+            type=intervallib.real_type_from_expr(vco, -(gain * w * w * gain)*(x), rel_prec=rel_prec))
+    dxdt = vco.decl_var("dxdt", kind=blocklib.VarKind.Transient, \
+            type=intervallib.real_type_from_expr(vco, v, rel_prec=rel_prec))
+    
+    expr = VarAssign(dvdt, -(gain * w * w * gain)*(x))
+
+    vco.decl_relation(expr)
+    vco.decl_relation(VarAssign(dxdt, v))
+    vco.decl_relation(VarAssign(out, x))
+    vco.decl_relation(Integrate(v, dvdt - d*v, timestep=timestep))
+    vco.decl_relation(Integrate(x, dxdt, timestep=timestep))
+    
+    return vco
---
+++ tests/test_vco.py
@@ -0,0 +1,113 @@
+import pythams.core.block as blocklib
+from pythams.core.expr import *
+import pythams.core.intervals as intervallib
+import pythams.core.fixedpoint as fxplib
+import pythams.core.integer as intlib 
+import pythams.core.rtl as rtllib 
+
+import matplotlib.pyplot as plt
+from pymtl3 import *
+import sys
+
+sys.path.insert(0, '../models')
+import vco as vco
+
+def validate_model(blk,timestep,figname):
+    print(blk)
+    xi,vi = 1.0,0.0
+    xs = []
+    vs = []
+    ts = []
+    cycles_per_sec = round(1/timestep)
+    max_cycles = 10000*cycles_per_sec
+    for t in range(max_cycles):
+    
+        values = blocklib.execute_block(blk,{"w":0.8, "x": xi, "v": vi})
+        ts.append(t*timestep)
+        xs.append(xi)
+        vs.append(vi)
+
+
+        
+        xi = values["x"]
+        vi = values["v"]
+
+    
+    print(len(vs))
+    xs.append(xi)
+    vs.append(vi)
+    ts.append(max_cycles*timestep)
+
+    plt.plot(ts,xs, label='xs')
+    plt.plot(ts,vs, label='vs')
+    plt.legend(loc='best')
+    plt.savefig(figname)
+    plt.clf()
+
+
+def validate_pymtl_model(rtlblk,timestep,figname):
+    outs = []
+    ts = []
+    cycles_per_sec = round(1/timestep)
+    max_cycles = 30*cycles_per_sec
+    for t in range(max_cycles):
+        values = rtlblk.pymtl_sim_tick({"w":Bits(rtlblk.block.get_var('w').type.nbits, v=rtlblk.scale_value_to_int(0.999,rtlblk.block.get_var('w').type))})
+        ts.append(t*timestep)
+        outi = values["out"]
+        outs.append(rtlblk.scale_value_to_real(outi, rtlblk.block.get_var('out').type))
+
+   
+
+    plt.plot(ts,outs)
+    plt.show()
+    #plt.savefig(figname)
+    #plt.clf()
+
+
+block = vco.generate_block()
+ival_reg = intervallib.compute_intervals_for_block(block,rel_prec=0.01)
+print("------ Real-valued AMS Block -----")
+print(block)
+print("\n")
+input("press any key to run simulation..")
+validate_model(block, 1e-2, "orig_dynamics.png")
+
+print(block.relations())
+
+raise(Exception)
+
+fp_block = fxplib.to_fixed_point(ival_reg, block)
+print("------ Fixed Point AMS Block -----")
+print(fp_block)
+print("\n")
+input("press any key to run simulation..")
+validate_model(fp_block, timestep, "fp_dynamics.png")
+
+print(fp_block.vars())
+for i in fp_block.relations():
+    print(i.pretty_print())
+
+#raise Exception
+
+
+int_block = intlib.to_integer(fp_block)
+print("------ Integer AMS Block -----")
+print(int_block)
+print("\n")
+input("press any key to run simulation..")
+validate_model(int_block, timestep, "int_dynamics.png")
+
+for i in int_block.relations():
+    print(i.pretty_print())
+
+for v in int_block.vars():
+    print(v)
+
+
+  
+rtl_block = rtllib.RTLBlock(int_block, {'x':0.79, 'v':0.00})
+
+rtl_block.generate_verilog_src("./")
+rtl_block.generate_pymtl_wrapper()
+rtl_block.pymtl_sim_begin()
+validate_pymtl_model(rtl_block,timestep,"rtl_dynamics.png")
